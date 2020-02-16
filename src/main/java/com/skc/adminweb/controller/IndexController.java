7e291827fa8115975054ad1e0e2f93be2e5ca6d1
KiChang Shin <kc7851@gmail.com>
2020-02-16 20:44:24 +0900

    init Project

---
+++ src/main/java/com/skc/adminweb/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.skc.adminweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "hello";
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello 12";
+    }
+}
