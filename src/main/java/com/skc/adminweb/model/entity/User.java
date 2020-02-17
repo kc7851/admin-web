f4df7e388dd46e33f5082a2ed2174f5d0ceec2fa
KiChang Shin <kc7851@gmail.com>
2020-02-17 23:51:02 +0900

    8. Entity & Repository

---
+++ src/main/java/com/skc/adminweb/model/entity/User.java
@@ -0,0 +1,35 @@
+package com.skc.adminweb.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String account;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private LocalDateTime createAt;
+
+    private String createBy;
+
+    private LocalDateTime updateAt;
+
+    private String updateBy;
+
+}
