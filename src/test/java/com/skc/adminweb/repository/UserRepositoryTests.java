c4d231df7ea0f08670fb971c5f8297cab5c6c0ff
KiChang Shin <kc7851@gmail.com>
2020-02-19 20:48:24 +0900

    10. JPA를 통한 CRUD 배우기 - 1_2

--- src/test/java/com/skc/adminweb/repository/UserRepositoryTests.java
+++ src/test/java/com/skc/adminweb/repository/UserRepositoryTests.java
@@ -8,6 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,13 +22,37 @@ public class UserRepositoryTests {
     @Test
     public void create() {
         User user = new User();
-        user.setAccount("test");
-        user.setEmail("test@exmaple.com");
-        user.setPhoneNumber("010-1111-1111");
+        user.setAccount("test2");
+        user.setEmail("test2@exmaple.com");
+        user.setPhoneNumber("010-1111-2222");
         user.setCreatedAt(LocalDateTime.now());
-        user.setCreatedBy("testUser");
+        user.setCreatedBy("testUser2");
 
         User save = repository.save(user);
         System.out.println("newUser: " + save);
+    }
+
+    @Test
+    public void read() {
+        Optional<User> user = repository.findById(2L);
+
+        user.ifPresent(selectUser -> {
+            System.out.println("User: " + selectUser);
+        });
+
+        assertThat(user.get().getAccount()).isEqualTo("test");
+    }
+
+    @Test
+    public void update() {
+        Optional<User> user = repository.findById(2L);
+
+        user.ifPresent(selectUser -> {
+           selectUser.setAccount("aaaa");
+           selectUser.setUpdatedAt(LocalDateTime.now());
+           selectUser.setUpdatedBy("update method()");
+
+           repository.save(selectUser);
+        });
     }
 }
