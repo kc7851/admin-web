732ca20b2174d507201bb32cd55d0cd34d6a6537
KiChang Shin <kc7851@gmail.com>
2020-02-23 21:26:05 +0900

    15. JPA 연관관계 설정 배우기 - QueryMethod
    fetch
    Lazy와 EAGER 차이

--- src/main/java/com/skc/adminweb/model/entity/Item.java
+++ src/main/java/com/skc/adminweb/model/entity/Item.java
@@ -23,6 +23,11 @@ public class Item {
 
     private String contents;
 
+    // Lazy = SELECT * FROM item where id = ? 따로 메소드를 호출하지 않은 이상 연관관계가 설정된 테이블에 대해서 select를 하지 않는다.
+    // 연관관계가 있는 경우 Lazy 사용
+    // EAGER = 연관관계가 있는 모든 테이블에 대해서 join이 일어난다. 데이터가 많이 걸려있으면 모든 데이터를 가져와서 성능의 저하가 있을 수 있다.
+    // 연관관계가 1:1인 경우 EAGER 사용
+
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "item")
     private List<OrderDetail> orderDetailList;
 }
--- src/main/java/com/skc/adminweb/repository/UserRepository.java
+++ src/main/java/com/skc/adminweb/repository/UserRepository.java
@@ -4,6 +4,18 @@ import com.skc.adminweb.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // select * from user where account = ?
+    Optional<User> findByAccount(String account);
+
+    // select * from user where email = ?
+    Optional<User> findByEmail(String email);
+
+    // select * from user where account = ? and email = ?
+    Optional<User> findByAccountAndEmail(String account, String email);
+
 }
