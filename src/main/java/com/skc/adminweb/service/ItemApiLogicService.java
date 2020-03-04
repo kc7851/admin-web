2d3c4f92ae2405cfa17719a411dd2fd1408d1e5d
KiChang Shin <kc7851@gmail.com>
2020-03-05 01:17:40 +0900

    8-2-04. 상품 API UPDATE

--- src/main/java/com/skc/adminweb/service/ItemApiLogicService.java
+++ src/main/java/com/skc/adminweb/service/ItemApiLogicService.java
@@ -45,13 +45,34 @@ public class ItemApiLogicService implements CurdInterface<ItemApiRequest, ItemAp
     @Override
     public Header<ItemApiResponse> read(Long id) {
         return itemRepository.findById(id)
-            .map(item -> response(item))
-            .orElseGet(() -> Header.ERROR("데이터 없음"));
+                .map(item -> response(item))
+                .orElseGet(() -> Header.ERROR("데이터 없음"));
     }
 
     @Override
     public Header<ItemApiResponse> update(Header<ItemApiRequest> request) {
-        return null;
+
+        ItemApiRequest body = request.getData();
+
+        return itemRepository.findById(body.getId())
+                .map(entityItem -> {
+                    entityItem
+                            .setStatus(body.getStatus())
+                            .setName(body.getName())
+                            .setTitle(body.getTitle())
+                            .setContent(body.getContent())
+                            .setPrice(body.getPrice())
+                            .setBrandName(body.getBrandName())
+                            .setRegisteredAt(body.getRegisteredAt())
+                            .setUnregisteredAt(body.getUnregisteredAt())
+                    ;
+
+                    return entityItem;
+                })
+                .map(newEntityItem ->
+                        itemRepository.save(newEntityItem))
+                .map(item -> response(item))
+                .orElseGet(() -> Header.ERROR("데이터 없음"));
     }
 
     @Override
